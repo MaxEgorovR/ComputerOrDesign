31df69718b172a700ce444f17be7c758e0fff2d7
Максим Егоров <truemalprem@gmail.com>
2024-12-22 14:34:13 +0300

    first commit

---
+++ Buider/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace Buider
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choise the computer:\n1 - Office\n2 - Budget\n3 - Game");
+            int choise = int.Parse(Console.ReadLine());
+            ComputerDirector director = new ComputerDirector(choise == 1 ? new OfficeComputerBuilder() : choise == 2 ? new BudgetComputerBuilder() : new GameComputerBuilder());
+            director.BuildComputer();
+            var character = director.GetCharacter();
+            Console.WriteLine(character.ToString());
+        }
+        public class Computer
+        {
+            public string CPU { get; set; }
+            public int RAM { get; set; }
+            public int ROM { get; set; }
+            public string GPU { get; set; }
+            public override string ToString()
+            {
+                return $"CPU: {CPU}\n" +
+                    $"RAM: {RAM}\n" +
+                    $"ROM: {ROM}\n" +
+                    $"GPU: {GPU}";
+            }
+        }
+
+        public abstract class ComputerBuilder
+        {
+            protected Computer computer = new Computer();
+            public abstract void SetCPU();
+            public abstract void SetGPU();
+            public abstract void SetRAM();
+            public abstract void SetROM();
+
+            public Computer GetResult()
+            {
+                return computer;
+            }
+        }
+
+        public class OfficeComputerBuilder : ComputerBuilder
+        {
+            public override void SetCPU()
+            {
+                computer.CPU = "zeon";
+            }
+
+            public override void SetGPU()
+            {
+                computer.GPU = "built-in";
+            }
+
+            public override void SetRAM()
+            {
+                computer.RAM = 8;
+            }
+
+            public override void SetROM()
+            {
+                computer.ROM = 512;
+            }
+        }
+
+        public class BudgetComputerBuilder : ComputerBuilder
+        {
+            public override void SetCPU()
+            {
+                computer.CPU = "i5";
+            }
+
+            public override void SetGPU()
+            {
+                computer.GPU = "discrete";
+            }
+
+            public override void SetRAM()
+            {
+                computer.RAM = 16;
+            }
+
+            public override void SetROM()
+            {
+                computer.ROM = 1024;
+            }
+        }
+
+        public class GameComputerBuilder : ComputerBuilder
+        {
+            public override void SetCPU()
+            {
+                computer.CPU = "i7";
+            }
+
+            public override void SetGPU()
+            {
+                computer.GPU = "discrete";
+            }
+
+            public override void SetRAM()
+            {
+                computer.RAM = 32;
+            }
+
+            public override void SetROM()
+            {
+                computer.ROM = 2048;
+            }
+        }
+
+        public class ComputerDirector
+        {
+            private ComputerBuilder builder;
+
+            public ComputerDirector(ComputerBuilder builder)
+            {
+                this.builder = builder;
+            }
+
+            public void BuildComputer()
+            {
+                builder.SetCPU();
+                builder.SetGPU();
+                builder.SetRAM();
+                builder.SetROM();
+            }
+
+            public Computer GetCharacter()
+            {
+                return builder.GetResult();
+            }
+        }
+    }
+}
---
+++ Fabrik/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace Fabrik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choise the design style:\n1 - Light\n2 - Dark\n3 - Minimal");
+            int choise = int.Parse(Console.ReadLine());
+            IDesignFactory factory = choise == 1 ? new LightFactory() : choise == 2 ? new DarkFactory() : new MinimalFactory();
+            var design = new Design(factory);
+            design.start();
+        }
+
+        public interface IButton
+        {
+            void Descreibe();
+        }
+
+        public interface IWindow
+        {
+            void Descreibe();
+        }
+
+        public class DarkButton : IButton
+        {
+            public void Descreibe()
+            {
+                Console.WriteLine("It's dark button now");
+            }
+        }
+
+        public class LightButton : IButton
+        {
+            public void Descreibe()
+            {
+                Console.WriteLine("It's light button now");
+            }
+        }
+
+        public class MinimalButton : IButton
+        {
+            public void Descreibe()
+            {
+                Console.WriteLine("It's minimal button now");
+            }
+        }
+
+        public class DarkWindow : IWindow
+        {
+            public void Descreibe()
+            {
+                Console.WriteLine("It's dark window now");
+            }
+        }
+
+        public class LightWindow : IWindow
+        {
+            public void Descreibe()
+            {
+                Console.WriteLine("It's light window now");
+            }
+        }
+
+        public class MinimalWindow : IWindow
+        {
+            public void Descreibe()
+            {
+                Console.WriteLine("It's minimal window now");
+            }
+        }
+
+        public interface IDesignFactory
+        {
+            public IButton create_button();
+            public IWindow create_window();
+        }
+
+        public class LightFactory : IDesignFactory
+        {
+            IButton IDesignFactory.create_button()
+            {
+                return new LightButton();
+            }
+
+            IWindow IDesignFactory.create_window()
+            {
+                return new LightWindow();
+            }
+        }
+
+        public class DarkFactory : IDesignFactory
+        {
+            IButton IDesignFactory.create_button()
+            {
+                return new DarkButton();
+            }
+
+            IWindow IDesignFactory.create_window()
+            {
+                return new DarkWindow();
+            }
+        }
+
+        public class MinimalFactory : IDesignFactory
+        {
+            IButton IDesignFactory.create_button()
+            {
+                return new MinimalButton();
+            }
+
+            IWindow IDesignFactory.create_window()
+            {
+                return new MinimalWindow();
+            }
+        }
+
+        public class Design
+        {
+            private IWindow _window;
+            private IButton _button;
+
+            public Design(IDesignFactory designFactory)
+            {
+                _window = designFactory.create_window();
+                _button = designFactory.create_button();
+            }
+
+            public void start()
+            {
+                Console.WriteLine("Wellcome!");
+                _button.Descreibe();
+                _window.Descreibe();
+            }
+        }
+    }
+}
